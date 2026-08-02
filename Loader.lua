b8fb6e5bcb6442e9719f267471645033228c4747
senzxyz2xxx <senzxyz2006@gmail.com>
2026-08-02 16:49:42 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,20 @@
+local Games = {
+    [107653945083776] = "https://raw.githubusercontent.com/senzxyz2xxx/SenzyHub/main/Maps/RollAnimeToFight.lua"
+}
+
+local currentPlaceId = game.PlaceId
+local scriptUrl = Games[currentPlaceId]
+
+if scriptUrl then
+    loadstring(game:HttpGet(scriptUrl))()
+else
+    local StarterGui = game:GetService("StarterGui")
+    pcall(function()
+        StarterGui:SetCore("SendNotification", {
+            Title = "Senzy Hub",
+            Text = "Game not supported! (Place ID: " .. tostring(currentPlaceId) .. ")",
+            Duration = 5
+        })
+    end)
+    warn("[Senzy Hub] Unsupported Game Place ID:", currentPlaceId)
+end
