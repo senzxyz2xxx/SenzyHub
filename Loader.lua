540e68bc9e5ed659087a8bfa5d752b9b09b32fc8
senzxyz2xxx <senzxyz2006@gmail.com>
2026-08-23 21:57:35 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,8 @@
 local Games = {
     [107653945083776] = "https://raw.githubusercontent.com/senzxyz2xxx/SenzyHub/refs/heads/main/Maps/senzyhub1.lua",
-    [133294838637122] = "https://raw.githubusercontent.com/senzxyz2xxx/SenzyHub/refs/heads/main/Maps/JumpToStealSoccerPlayers.lua",
+    [133294838637122]= "https://raw.githubusercontent.com/senzxyz2xxx/SenzyHub/refs/heads/main/Maps/JumpToStealSoccerPlayers.lua",
+    [133623616308412]= "https://raw.githubusercontent.com/senzxyz2xxx/SenzyHub/refs/heads/main/Maps/JumpToStealSoccerPlayers.lua",
+
 }
 
 local currentPlaceId = game.PlaceId
